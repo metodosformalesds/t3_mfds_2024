27eacbfe0238d091b7ef4aa463d2f907909c9077
192880 Brandon Hernandez <al192880@alumnos.uacj.mx>
2024-10-15 14:10:06 -0600

    Agregando las apps

---
+++ ecommerce/apps/autenticacion_usuarios/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AutenticacionUsuariosConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'autenticacion_usuarios'
---
+++ ecommerce/apps/carrito_compra/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CarritoCompraConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'carrito_compra'
