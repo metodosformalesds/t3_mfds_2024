0113d06e8fd61b2c2bd493a70775a638896ad2fa
192880 Brandon Hernandez <al192880@alumnos.uacj.mx>
2024-10-16 21:33:05 -0600

    .

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ecommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["C:/Users/BHerna12/OneDrive - JNJ/Documents/ECOMMERCE/t3_mfds_2024/ecommerce/ecommerce/plantillas"],
+        'DIRS': ["ecommerce/plantillas"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
