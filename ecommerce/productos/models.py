3ea558b3635c12a6b99b0954d71d199fcaf041d5
BrandonChao12 <143544552+BrandonChao12@users.noreply.github.com>
2024-10-23 17:47:33 -0600

    model products

--- ecommerce/productos/models.py
+++ ecommerce/productos/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class AutoPart(models.Model):
+    Name = models.CharField(max_length=255)
+    Description = models.TextField()
+    Price = models.DecimalField(max_digits=10, decimal_places=2)  # Precio con 2 decimales
+    Condition = models.CharField(max_length=50)  # Condición (Ej: 'Nuevo', 'Usado')
+    Brand = models.CharField(max_length=100)
+    Model = models.CharField(max_length=100)
+    Year = models.IntegerField()  # Año del auto
+    CarPartType = models.CharField(max_length=100)  # Tipo de autoparte (Ej: 'Motor', 'Llantas')
+    Image = models.ImageField(upload_to='autoparts/images/')  # Ruta de la imagen
+    Stock = models.IntegerField()  # Cantidad en inventario
+
+    def __str__(self):
+        return self.Name
