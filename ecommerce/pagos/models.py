30454ce9e131159d3cf4b86f927e8607f9d18f0b
BrandonChao12 <143544552+BrandonChao12@users.noreply.github.com>
2024-10-23 17:53:56 -0600

    model transaction

--- ecommerce/pagos/models.py
+++ ecommerce/pagos/models.py
@@ -10,3 +10,16 @@ class Subscription(models.Model):
 
     def __str__(self):
         return f"{self.SubscriptionType} - {self.SellerID.name}"
+
+class Transaction(models.Model):
+    TransactionID = models.AutoField(primary_key=True)
+    BuyerID = models.ForeignKey(Buyer, on_delete=models.CASCADE)  # Relación con Buyer
+    SellerID = models.ForeignKey(Seller, on_delete=models.CASCADE)  # Relación con Seller
+    AutoPartID = models.ForeignKey(AutoPart, on_delete=models.CASCADE)  # Relación con AutoPart
+    Date = models.DateTimeField()  # Fecha y hora de la transacción
+    TotalAmount = models.DecimalField(max_digits=10, decimal_places=2)  # Monto total con 2 decimales
+    PaymentMethod = models.CharField(max_length=50)  # Método de pago (Ej: 'Tarjeta', 'Paypal')
+    TransactionStatus = models.CharField(max_length=50)  # Estado de la transacción (Ej: 'Completada', 'Pendiente')
+
+    def __str__(self):
+        return f"Transaction {self.TransactionID} - {self.BuyerID.name} to {self.SellerID.name}"
