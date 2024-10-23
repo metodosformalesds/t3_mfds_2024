d22e40214b38cd0e30af859d5435694993e54ca5
BrandonChao12 <143544552+BrandonChao12@users.noreply.github.com>
2024-10-23 17:51:10 -0600

    model suscription

--- ecommerce/pagos/models.py
+++ ecommerce/pagos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Subscription(models.Model):
+    SubscriptionID = models.AutoField(primary_key=True)
+    SellerID = models.ForeignKey(Seller, on_delete=models.CASCADE)  # Relación con Seller
+    SubscriptionType = models.CharField(max_length=100)  # Tipo de suscripción (Ej: 'Básico', 'Premium')
+    Price = models.DecimalField(max_digits=10, decimal_places=2)  # Precio con dos decimales
+    StartDate = models.DateField()  # Fecha de inicio de la suscripción
+    EndDate = models.DateField()  # Fecha de fin de la suscripción
+
+    def __str__(self):
+        return f"{self.SubscriptionType} - {self.SellerID.name}"
